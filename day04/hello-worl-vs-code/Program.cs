99b413122a0de431275edfa6e8a57001964f0ea1
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-07-03 12:32:52 -0400

    .

---
+++ day04/hello-worl-vs-code/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Sadeed!");
+Console.WriteLine(60+20);
+int num1 = 70;
+int num2 = 60;
+int result = num1 + num2;
+Console.WriteLine(result);
---
+++ day04/hello-world-vs/hello-world/hello-world/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(30+20);
+int num1 = 30;
+int num2 = 60;
+int result = num1 + num2;
+Console.WriteLine(result);
+
