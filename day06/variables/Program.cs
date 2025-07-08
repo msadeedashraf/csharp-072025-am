c841b61512c49f6914f048f4a85f821eca347bad
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-07-08 12:16:15 -0400

    .

---
+++ day06/variables/Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+int x = 50;
+int y = 90;
+int result = x + y;
+Console.WriteLine(result);
+*/
+
+/*
+int x, y, z;
+x = 10;
+y = 50;
+z = 60;
+
+Console.WriteLine(z+y-x);
+*/
+/*
+int x, y, z;
+
+x=y=z = 10;
+
+Console.WriteLine(z+y+x);
+*/
+
+/*
+int myBillAmount = 100;
+int tipAmount = 15;
+int totalBillAmount = myBillAmount + tipAmount;
+Console.WriteLine("My Total Bill is " + totalBillAmount);
+*/
+
+//Tip Calculator
+/*
+Console.WriteLine("Tip Amount\n\n\n");
+Console.WriteLine("##########\n\n\n");
+Console.Write("Please input the Bill Amount: ");
+string billAmount = Console.ReadLine();
+Console.Write("Please input the Tip Amount: ");
+string tipAmount = Console.ReadLine();
+
+
+//int totalBillAmount =  Convert.ToInt32(billAmount) + Convert.ToInt32(tipAmount);
+
+int totalBillAmount = int.Parse(billAmount) + int.Parse(tipAmount);
+
+Console.WriteLine("My Total Bill is " + totalBillAmount);
+*/
+
+//Loan Calculator
+            Console.WriteLine("Loan Calculator\n");
+            Console.WriteLine("##########\n");
+            Console.Write("Please input the Loan Amount: ");
+            string loanAmountInput = Console.ReadLine();
+            double loanAmount = double.Parse(loanAmountInput);
+
+
+            Console.Write("Please input the annual Rate %: ");
+            string rateInput = Console.ReadLine();
+            double annualRate = double.Parse(rateInput);
+
+
+            Console.Write("Please input the No. of Months: ");
+            string monthsInput = Console.ReadLine();
+            double months = double.Parse(monthsInput);
+
+double monthlyRate = (annualRate / 100) / 12;
+
+double monthlyPayment = loanAmount * 
+                             (monthlyRate * Math.Pow(1 + monthlyRate, months)) / 
+                             (Math.Pow(1 + monthlyRate, months) - 1);
+        
+
+            Console.WriteLine("Your Monthly Payment is : " + monthlyPayment);
+        
---
+++ day07/string/string/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Strings
+
+string myFullName = "Muhammad Sadeed Ashraf";
+
+Console.WriteLine(myFullName.Length);
+
+Console.WriteLine(myFullName.Replace("Sadeed", "S"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Console.WriteLine("Hello, World!");
+/*
+//Currency Calculator
+using Microsoft.VisualBasic;
+
+double myUSD = 100;
+double myCAD = 100;
+double coversion = 0.78;
+
+Console.WriteLine("Currency Convertor");
+Console.WriteLine("Please input the USD amount.");
+
+myUSD = Convert.ToDouble(Console.ReadLine());
+
+myCAD = myUSD / coversion;
+
+//Console.WriteLine(myUSD + " USD Converted into " + Math.Round(myCAD,2) + " CAD");
+
+// String Interpolation 
+Console.WriteLine($"{myUSD} USD Converted into {Math.Round(myCAD, 2)} CAD");
+*/
+
+/*
+//Age Calculator
+
+
+
+DateTime todayDate = DateTime.Now;
+
+int currentYear = todayDate.Year;
+
+
+Console.WriteLine("Please enter your DOB");
+
+DateTime myDOB = Convert.ToDateTime(Console.ReadLine())    ;
+
+int myDOBYear = myDOB.Year;
+
+int myAge = currentYear - myDOBYear ;
+
+Console.Write($"Your age seems to be {myAge} years");
+
+
+//Console.WriteLine(myDOB.Year);
+//Console.WriteLine(myDOB.Month);
+//Console.WriteLine(myDOB.Day);
+
+*/
+
+
+
