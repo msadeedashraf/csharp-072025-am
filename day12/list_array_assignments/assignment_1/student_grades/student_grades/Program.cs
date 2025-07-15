e3d59acc646fb1f12065588870beb3d941865539
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-07-15 12:04:29 -0400

    .

---
+++ day12/list_array_assignments/assignment_1/student_grades/student_grades/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter number of students:");
+
+int numStudents = int.Parse(Console.ReadLine());
+
+int[] grades = new int[numStudents];
+
+for (int i = 0; i < numStudents; i++)
+{
+    Console.WriteLine($"Enter the grades for sudent {i+1}");
+    grades[i] = int.Parse(Console.ReadLine());
+}
+
+int sum = 0;
+int min = grades[0];
+int max = grades[0];
+
+foreach(int grade in grades)
+{
+    sum += grade;
+    if(grade < min) min = grade;
+    if (grade > max) max = grade;
+}
+
+double average = (double)sum / numStudents;
+
+Console.WriteLine($"\n Average grade : {average:F2}");
+Console.WriteLine($"Highest grade: {max}");
+Console.WriteLine($"Lowest grade: {min}");
+
+Console.WriteLine("All grades: ");
+foreach(int grade in grades)
+{
+    Console.WriteLine(grade);
+}
---
+++ day12/list_array_assignments/assignment_2/to-do-list/to-do-list/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<string> tasks = new List<string>();
+
+bool running = true;
+
+while (running)
+{
+    Console.WriteLine("\n--- To-Do List Menu ---");
+    Console.WriteLine("1. Add a new task");
+    Console.WriteLine("2. Remove a task");
+    Console.WriteLine("3. View all tasks");
+    Console.WriteLine("4. Exit");
+    Console.Write("Choose an option: ");
+
+    string choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            Console.WriteLine("Enter new task");
+            string newTask = Console.ReadLine();
+            tasks.Add(newTask);
+            Console.WriteLine("Task added successfully.");
+            break;
+        case "2":
+            Console.WriteLine("Enter the task to be removed");
+            string removeTask = Console.ReadLine();
+            if (tasks.Remove(removeTask))
+                Console.WriteLine("Task removed");
+            else
+                Console.WriteLine("Task not Found");
+            break;
+        case "3":
+            Console.WriteLine("\n Your Tasks:");
+            foreach (var task in tasks)
+                Console.WriteLine($"\n {task}");
+
+            if (tasks.Count == 0)
+                Console.WriteLine("No tasks yet");
+            break;
+        case "4":
+            running = false;
+            break;
+
+        default:
+            Console.WriteLine("Invalid Option. Try again");
+            break;
+
+
+    }
+
+
+
+}
+
+Console.WriteLine("Goodbye!");
+
